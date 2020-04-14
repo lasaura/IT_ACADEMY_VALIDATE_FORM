e3a3e4a3d87637e4af888e27b2243979d9be93d3
lasaura <37713803+lasaura@users.noreply.github.com>
2020-04-14 13:07:44 +0200

    add files with class style error and Noerror

--- js/scripts.js
+++ js/scripts.js
@@ -18,7 +18,7 @@ function Validate() {
 
   // validate username
   if (username.value == "") {
-    username.style.border = "1px solid red";
+    username.classList.add("inputError");
     document.getElementById('username_div').style.color = "red";
     name_error.textContent = "Username is required";
     username.focus();
@@ -28,55 +28,56 @@ function Validate() {
   }
   // validate username
   if (username.value.length < 3) {
-    username.style.border = "1px solid red";
+    username.classList.add("inputError");
     document.getElementById('username_div').style.color = "red";
     name_error.textContent = "Username must be at least 3 characters";
     username.focus();
     count_error++;
   }else {
-    username.style.border = "1px solid black";
+    username.classList.add("inputNoError");
     document.getElementById('username_div').style.color = "black";
     name_error.textContent = "";
   }
+
   // validate email
   var regex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;;
   //if (email.value == "") {
     if (!(regex.test(email.value))) {
-    email.style.border = "1px solid red";
-    document.getElementById('email_div').style.color = "red";
+    email.classList.add("inputError");
+    document.getElementById('email_div').style.color="red";
     email_error.textContent = "Email is required";
     email.focus();
     count_error++;
   } else {
-    email.style.border = "1px solid black";
-    document.getElementById('email_div').style.color = "black";
+    email.classList.add("inputNoError");
+    document.getElementById('email_div').style.color="black";
     email_error.textContent = "";
   }
   // validate password
   if (password.value == "") {
-    password.style.border = "1px solid red";
+    password.classList.add("inputError");
     document.getElementById('password_div').style.color = "red";
-    password_confirm.style.border = "1px solid red";
+    password_confirm.classList.add("inputError");
     password_error.textContent = "Password is required";
     password.focus();
     count_error++;
   }else {
-    password.style.border = "1px solid black";
+    password.classList.add("inputNoError");
     document.getElementById('password_div').style.color = "black";
-    password_confirm.style.border = "1px solid black";
+    password_confirm.classList.add("inputNoError");
     password_error.textContent = "";
   }
   // check if the two passwords match
   if (password.value != password_confirm.value) {
-    password.style.border = "1px solid red";
+    password.classList.add("inputError");
     document.getElementById('pass_confirm_div').style.color = "red";
-    password_confirm.style.border = "1px solid red";
+    password_confirm.classList.add("inputError");
     password_error.innerHTML = "The two passwords do not match";
     count_error++;
   }else{
-    password.style.border = "1px solid black";
+    password.classList.add("inputNoError");
     document.getElementById('pass_confirm_div').style.color = "black";
-    password_confirm.style.border = "1px solid black";
+    password_confirm.classList.add("inputNoError");
     password_error.innerHTML = "";
   }
 
